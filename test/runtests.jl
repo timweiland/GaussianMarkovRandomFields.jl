9a77dd62e8896fb24f1f3b0bbe6f5a78810a7c71
Tim Weiland <tim@weiland-lahnstein.de>
2024-07-12 17:02:15 +0200

    Change ambiguity detection mechanism
    Aqua.jl - for some reason - also detects
    ambiguities in our dependencies. We do not care
    about those. So we're switching to the ambiguity
    detection mechanism provided by `Test`.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,8 @@ using JET
 
 @testset "GMRFs.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(GMRFs)
+        Aqua.test_all(GMRFs; ambiguities = false)
+        @test length(Test.detect_ambiguities(GMRFs)) == 0
     end
     @testset "Code linting (JET.jl)" begin
         JET.test_package(GMRFs; target_defined_modules = true)
