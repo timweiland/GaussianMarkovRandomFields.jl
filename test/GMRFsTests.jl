4ac799ef16f20375802b1a5db8fdd2572dcaa11a
Tim Weiland <tim@weiland-lahnstein.de>
2024-07-19 23:23:33 +0200

    Remove JET
    Seems to not play nice with our ReTest-based
    setup. Worth trying to get it working again in the
    future, but for now, it's too much of a hassle.

--- test/GMRFsTests.jl
+++ test/GMRFsTests.jl
@@ -3,7 +3,6 @@ module GMRFTests
 using GMRFs
 using ReTest
 using Aqua
-using JET
 
 include("test_gmrf.jl")
 include("spdes/test_fem_discretization.jl")
@@ -12,9 +11,6 @@ include("spdes/test_matern.jl")
 @testset "Code quality (Aqua.jl)" begin
     Aqua.test_all(GMRFs; ambiguities = false)
     @test length(Test.detect_ambiguities(GMRFs)) == 0
-end
-@testset "Code linting (JET.jl)" begin
-    JET.test_package(GMRFs; target_defined_modules = true)
 end
 
 end
