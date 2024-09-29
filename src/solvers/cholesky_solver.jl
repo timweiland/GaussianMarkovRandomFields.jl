3d1e3795732e85f297f124028ed1cf6cf112ff4d
Tim Weiland <tim@weiland-lahnstein.de>
2024-09-29 17:44:43 +0200

    Ensure dense vecs in Cholesky solver

--- src/solvers/cholesky_solver.jl
+++ src/solvers/cholesky_solver.jl
@@ -2,6 +2,9 @@ using SparseArrays, LinearAlgebra, Distributions
 
 export CholeskySolver, CholeskySolverBlueprint
 
+_ensure_dense(x) = x
+_ensure_dense(x::SparseVector) = Array(x)
+
 struct CholeskySolver{G<:AbstractGMRF,V<:AbstractVarianceStrategy} <: AbstractSolver
     gmrf::G
     var_strategy::V
@@ -22,8 +25,9 @@ end
 
 function compute_mean(s::CholeskySolver{<:LinearConditionalGMRF})
     x = s.gmrf
-    residual = x.y - (x.A * mean(x.prior) + x.b)
-    return mean(x.prior) + s.precision_chol \ (x.A' * (x.Q_ϵ * residual))
+    μ = _ensure_dense(mean(x.prior))
+    residual = x.y - (x.A * μ + x.b)
+    return μ + s.precision_chol \ _ensure_dense(x.A' * (x.Q_ϵ * residual))
 end
 
 function compute_variance(s::CholeskySolver)
@@ -32,8 +36,8 @@ end
 
 function compute_rand!(s::CholeskySolver, rng::Random.AbstractRNG, x::AbstractVector)
     randn!(rng, x)
-    x .= s.precision_chol.UP \ x
-    x .+= mean(s.gmrf)
+    x .= s.precision_chol.UP \ _ensure_dense(x)
+    x .+= _ensure_dense(mean(s.gmrf))
     return x
 end
 
