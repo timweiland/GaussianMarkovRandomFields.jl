b22ec8844f274b9077bd1307eca723ef96621ae9
Tim Weiland <tim@weiland-lahnstein.de>
2024-10-04 18:54:34 +0200

    Improve mesh generation utility

--- src/mesh_utils.jl
+++ src/mesh_utils.jl
@@ -1,6 +1,6 @@
 using Gmsh, FerriteGmsh, Ferrite
 
-export create_inflated_rectangle
+export create_inflated_rectangle, create_rectangle
 
 """
     create_inflated_rectangle(x0, y0, dx, dy, boundary_width, interior_mesh_size,
@@ -34,7 +34,7 @@ function create_inflated_rectangle(
     interior_mesh_size,
     exterior_mesh_size = 2 * interior_mesh_size;
     element_order = 1,
-)::Tuple{Ferrite.Grid,Vector{Int}}
+)
     Gmsh.initialize()
 
     gmsh.model.add("t1")
@@ -51,6 +51,7 @@ function create_inflated_rectangle(
 
     c_int = gmsh.model.geo.addCurveLoop([l_int_1, l_int_2, l_int_3, l_int_4])
     s_int = gmsh.model.geo.addPlaneSurface([c_int])
+    gmsh.model.geo.mesh.setTransfiniteSurface(s_int, "Left")
 
     p_ext_1 = gmsh.model.geo.addPoint(
         x0 - boundary_width,
@@ -89,8 +90,82 @@ function create_inflated_rectangle(
 
     group = gmsh.model.addPhysicalGroup(2, [s_ext, s_int])
     gmsh.model.setPhysicalName(2, group, "Domain")
+    int_group = gmsh.model.addPhysicalGroup(2, [s_int])
+    gmsh.model.setPhysicalName(2, int_group, "Interior")
     boundary_group = gmsh.model.addPhysicalGroup(1, [l_int_1, l_int_2, l_int_3, l_int_4])
-    gmsh.model.setPhysicalName(1, boundary_group, "Boundary")
+    gmsh.model.setPhysicalName(1, boundary_group, "Interior boundary")
+
+    ext_boundary_group = gmsh.model.addPhysicalGroup(1, [l_ext_1, l_ext_2, l_ext_3, l_ext_4])
+    gmsh.model.setPhysicalName(1, ext_boundary_group, "Exterior boundary")
+
+    gmsh.model.mesh.generate(2)
+    if element_order != 1
+        gmsh.model.mesh.setOrder(element_order)
+    end
+
+    # gmsh.fltk.run()
+
+
+    dim = Int64(gmsh.model.getDimension())
+    facedim = dim - 1
+
+    gmsh.model.mesh.renumberNodes()
+    gmsh.model.mesh.renumberElements()
+
+    # transfer the gmsh information
+    nodes = FerriteGmsh.tonodes()
+    elements, gmsh_elementidx = toelements(dim)
+    cellsets = tocellsets(dim, gmsh_elementidx)
+
+    # "Domain" is the name of a PhysicalGroup and saves all cells that define the computational domain
+    domaincellset = cellsets["Domain"]
+    elements = elements[collect(domaincellset)]
+
+    boundarydict = toboundary(facedim)
+    facesets = tofacetsets(boundarydict, elements)
+
+    # # boundary_tags, _ = gmsh.model.mesh.getNodesForPhysicalGroup(1, 2)
+    # # boundary_tags = map(x -> Int(x), boundary_tags)
+
+    gmsh.finalize()
+
+    return Ferrite.Grid(elements, nodes, facesets = facesets, cellsets = cellsets)
+end
+
+function create_rectangle(
+    x0,
+    y0,
+    dx,
+    dy,
+    interior_mesh_size;
+    element_order = 1,
+)
+    Gmsh.initialize()
+
+    gmsh.model.add("t1")
+
+    p_int_1 = gmsh.model.geo.addPoint(x0, y0, 0, interior_mesh_size)
+    p_int_2 = gmsh.model.geo.addPoint(x0 + dx, y0, 0, interior_mesh_size)
+    p_int_3 = gmsh.model.geo.addPoint(x0 + dx, y0 + dy, 0, interior_mesh_size)
+    p_int_4 = gmsh.model.geo.addPoint(x0, y0 + dy, 0, interior_mesh_size)
+
+    l_int_1 = gmsh.model.geo.addLine(p_int_1, p_int_2)
+    l_int_2 = gmsh.model.geo.addLine(p_int_2, p_int_3)
+    l_int_3 = gmsh.model.geo.addLine(p_int_3, p_int_4)
+    l_int_4 = gmsh.model.geo.addLine(p_int_4, p_int_1)
+
+    c_int = gmsh.model.geo.addCurveLoop([l_int_1, l_int_2, l_int_3, l_int_4])
+    s_int = gmsh.model.geo.addPlaneSurface([c_int])
+    gmsh.model.geo.mesh.setTransfiniteSurface(s_int, "Left")
+
+    gmsh.model.geo.synchronize()
+
+    group = gmsh.model.addPhysicalGroup(2, [s_int])
+    gmsh.model.setPhysicalName(2, group, "Domain")
+    int_group = gmsh.model.addPhysicalGroup(2, [s_int])
+    gmsh.model.setPhysicalName(2, int_group, "Interior")
+    boundary_group = gmsh.model.addPhysicalGroup(1, [l_int_1, l_int_2, l_int_3, l_int_4])
+    gmsh.model.setPhysicalName(1, boundary_group, "Interior boundary")
 
     gmsh.model.mesh.generate(2)
     if element_order != 1
@@ -115,11 +190,6 @@ function create_inflated_rectangle(
     boundarydict = toboundary(facedim)
     facesets = tofacetsets(boundarydict, elements)
 
-    boundary_tags, _ = gmsh.model.mesh.getNodesForPhysicalGroup(1, 2)
-    boundary_tags = map(x -> Int(x), boundary_tags)
-
     gmsh.finalize()
-
-    return Ferrite.Grid(elements, nodes, facesets = facesets, cellsets = cellsets),
-    boundary_tags
+    return Ferrite.Grid(elements, nodes, facesets = facesets, cellsets = cellsets)
 end
