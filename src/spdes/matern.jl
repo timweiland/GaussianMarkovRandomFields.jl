816c7aadf394aa4a090a2aa7c2fb1d770b5bdff3
Tim Weiland <tim@weiland-lahnstein.de>
2024-10-08 00:56:33 +0200

    Move product matern

--- src/spdes/matern.jl
+++ src/spdes/matern.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 using SparseArrays
 using SpecialFunctions
 
-export MaternSPDE, α, ndim, discretize, assemble_C_G_matrices, lump_matrix
+export MaternSPDE, α, ndim, discretize, assemble_C_G_matrices, product_matern
 
 ################################################################################
 #    Whittle-Matérn
@@ -165,4 +165,28 @@ function discretize(
         return ConstrainedGMRF(x, discretization.constraint_handler)
     end
     return x
+end
+
+function product_matern(
+    matern_temporal::MaternSPDE,
+    N_t::Int,
+    matern_spatial::MaternSPDE,
+    spatial_disc::FEMDiscretization;
+    solver_blueprint=DefaultSolverBlueprint(),
+)
+    offset = N_t ÷ 10
+    temporal_grid = generate_grid(Line, (N_t + 2*offset - 1,))
+    temporal_ip = Lagrange{1, RefCube, 1}()
+    temporal_qr = QuadratureRule{1, RefCube}(2)
+    temporal_disc = FEMDiscretization(temporal_grid, temporal_ip, temporal_qr)
+    x_t = discretize(matern_temporal, temporal_disc)
+
+    Q_t = to_matrix(precision_map(x_t))[offset+1:end-offset, offset+1:end-offset]
+    x_s = discretize(matern_spatial, spatial_disc)
+    Q_s = to_matrix(precision_map(x_s))
+
+    x_spatiotemporal = kronecker_product_spatiotemporal_model(Q_t, Q_s, spatial_disc; solver_blueprint=solver_blueprint)
+    if length(disc.constraint_handler.prescribed_dofs) > 0
+        return ConstrainedGMRF(x_spatiotemporal, disc.constraint_handler)
+    end
 end
