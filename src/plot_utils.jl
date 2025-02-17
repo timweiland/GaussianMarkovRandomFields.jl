ba3bb629314eb8c21789fad4ee3790e45a9f6587
Tim Weiland <tim@weiland-lahnstein.de>
2025-02-17 15:05:00 +0100

    Better 1D plot utils

--- src/plot_utils.jl
+++ src/plot_utils.jl
@@ -1,10 +1,68 @@
 using Ferrite, Makie
 
-export plot_spde_gmrf, plot_spatiotemporal_gmrf
+export plot_spde_gmrf, plot_spatiotemporal_gmrf, plot_spatiotemporal_gmrf_at_time
 
 # COV_EXCL_START
+
 ### 1D ###
+
+function _plot_1d_gaussian!(
+    ax,
+    plot_points,
+    means,
+    lower,
+    upper,
+    samples;
+    mean_color = :blue,
+    conf_band_color = (:blue, 0.3),
+    sample_color = (:gray, 0.3),
+)
+    lines!(ax, plot_points, means, color = mean_color)
+    band!(ax, plot_points, lower, upper, color = conf_band_color)
+    for samp in samples
+        lines!(ax, plot_points, samp, color = sample_color)
+    end
+end
+
 function plot_spde_gmrf(
+    x::AbstractGMRF,
+    disc::FEMDiscretization{1};
+    combined = true,
+    mean_pos = (1, 1),
+    std_pos = (1, 2),
+    sample_pos = [(2, 1), (2, 2)],
+    N_samples = 3,
+    limits = nothing,
+    compute_std = true,
+    field = :default,
+    rng = Random.default_rng(),
+)
+    if combined
+        return _plot_spde_gmrf_combined(
+            x,
+            disc,
+            limits = limits,
+            compute_std = compute_std,
+            N_samples = N_samples,
+            field = field,
+            rng = rng,
+        )
+    else
+        return _plot_spde_gmrf_separate(
+            x,
+            disc,
+            mean_pos = mean_pos,
+            std_pos = std_pos,
+            sample_pos = sample_pos,
+            limits = limits,
+            compute_std = compute_std,
+            field = field,
+            rng = rng,
+        )
+    end
+end
+
+function _plot_spde_gmrf_separate(
     d::AbstractGMRF,
     disc::FEMDiscretization{1};
     mean_pos = (1, 1),
@@ -13,6 +71,7 @@ function plot_spde_gmrf(
     limits = nothing,
     compute_std = true,
     field = :default,
+    rng = Random.default_rng(),
 )
     node_coords = map(n -> n.x[1], disc.grid.nodes)
     bounds = (minimum(node_coords), maximum(node_coords))
@@ -28,7 +87,7 @@ function plot_spde_gmrf(
     end
     means = eval_mat * means
     stds = eval_mat * stds
-    samples = [full_rand(Random.default_rng(), d) for _ in sample_pos]
+    samples = [rand(rng, d) for _ in sample_pos]
     samples = [eval_mat * s for s in samples]
 
     if limits === nothing
@@ -46,6 +105,43 @@ function plot_spde_gmrf(
     return fig
 end
 
+function _plot_spde_gmrf_combined(
+    d::AbstractGMRF,
+    disc::FEMDiscretization{1};
+    limits = nothing,
+    compute_std = true,
+    field = :default,
+    N_samples = 3,
+    rng = Random.default_rng(),
+)
+    node_coords = map(n -> n.x[1], disc.grid.nodes)
+    bounds = (minimum(node_coords), maximum(node_coords))
+    plot_points = range(bounds[1], bounds[2], length = 10 * length(node_coords))
+
+    eval_mat = evaluation_matrix(disc, [Tensors.Vec(x) for x in plot_points]; field = field)
+
+    means = eval_mat * mean(d)
+    if compute_std
+        stds = std(d)
+    else
+        stds = spzeros(size(means))
+    end
+    stds = eval_mat * stds
+    samples = [eval_mat * rand(rng, d) for _ = 1:N_samples]
+
+    confs = 1.96 * stds
+    upper = means + confs
+    lower = means - confs
+
+    if limits === nothing
+        limits = (nothing, nothing)
+    end
+    fig = Figure()
+    ax = Axis(fig[1, 1]; limits = limits)
+    _plot_1d_gaussian!(ax, plot_points, means, lower, upper, samples)
+    return fig
+end
+
 ST_GMRF_1D = Union{
     ConstantMeshSTGMRF{1},
     LinearConditionalGMRF{<:ConstantMeshSTGMRF{1}},
@@ -54,6 +150,45 @@ ST_GMRF_1D = Union{
 }
 
 ### 1D ###
+function plot_spatiotemporal_gmrf_at_time(
+    x::ST_GMRF_1D,
+    t::Int;
+    limits = nothing,
+    compute_std = true,
+    N_samples = 3,
+    field = :default,
+    rng = Random.default_rng(),
+)
+    disc = discretization_at_time(x, t)
+
+    node_coords = map(n -> n.x[1], disc.grid.nodes)
+    bounds = (minimum(node_coords), maximum(node_coords))
+    plot_points = range(bounds[1], bounds[2], length = 10 * length(node_coords))
+
+    eval_mat = evaluation_matrix(disc, [Tensors.Vec(x) for x in plot_points]; field = field)
+
+    means = time_means(x)[t]
+    if compute_std
+        stds = time_stds(x)[t]
+    else
+        stds = spzeros(size(means))
+    end
+    means = eval_mat * means
+    stds = eval_mat * stds
+    samples = [eval_mat * time_rands(x, rng)[t] for _ = 1:N_samples]
+    confs = 1.96 * stds
+    upper = means + confs
+    lower = means - confs
+
+    if limits === nothing
+        limits = (nothing, nothing)
+    end
+    fig = Figure()
+    ax = Axis(fig[1, 1]; limits = limits)
+    _plot_1d_gaussian!(ax, plot_points, means, lower, upper, samples)
+    return fig
+end
+
 function _plot_spatiotemporal_gmrf_separate(
     x::ST_GMRF_1D;
     mean_pos = (1, 1),
@@ -152,11 +287,7 @@ function _plot_spatiotemporal_gmrf_combined(
     end
     fig = Figure()
     ax = Axis(fig[1, 1]; limits = limits)
-    lines!(ax, plot_points, cur_means, color = :blue)
-    band!(ax, plot_points, lower, upper, color = :blue, alpha = 0.3)
-    for (i, sample) in enumerate(samples)
-        lines!(ax, plot_points, cur_samples[i], color = :gray, alpha = 0.3)
-    end
+    _plot_1d_gaussian!(ax, plot_points, cur_means, lower, upper, cur_samples)
 
     time_slider = Makie.Slider(fig[2, 1], range = 1:length(means), startvalue = 1)
     on(time_slider.value) do val
