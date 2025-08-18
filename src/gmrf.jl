4fe29eba73ff99cf0e2db69c1e466610e61c1e51
Tim Weiland <tim@weiland-lahnstein.de>
2025-08-18 23:59:47 +0300

    Refactor LinearSolve utilities into separate files
    - Move symmetrize utilities from gmrf.jl to src/utils/symmetrize.jl
    - Add src/utils/linsolve_utils.jl with prepare_for_linsolve and configure_algorithm
    - Update src/utils/utils.jl to include new utility files
    - Clean up gmrf.jl by removing moved utilities

--- src/gmrf.jl
+++ src/gmrf.jl
@@ -17,18 +17,6 @@ using SparseArrays
 using LinearMaps
 using LinearSolve
 
-"""
-    symmetrize(A)
-
-Apply appropriate symmetric wrapper to matrix types.
-- `Diagonal`: Return as-is (no wrapping needed)
-- `Tridiagonal`: Convert to `SymTridiagonal`
-- Others: Wrap in `Symmetric`
-"""
-symmetrize(A::Diagonal) = A
-symmetrize(A::SymTridiagonal) = A
-symmetrize(A::Tridiagonal) = SymTridiagonal(A)
-symmetrize(A::AbstractMatrix) = Symmetric(A)
 
 export AbstractGMRF, GMRF, precision_map, precision_matrix, InformationVector, information_vector
 
